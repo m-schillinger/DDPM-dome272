92e0fda8c1fa54520caf440e5a069c0dda4a3403
Maybritt Schillinger <maybritt.schillinger@stat.math.ethz.ch>
2023-01-14 20:48:32 +0100

    updated code for different resolution ratios

--- ddpm_downscale.py
+++ ddpm_downscale.py
@@ -221,7 +221,7 @@ def launch():
         args.c_out = 1
         if args.image_size is None:
             args.image_size = 32
-    args.run_name = f"NewTransform_NewSampling_fixed_s-{args.dataset_size}_train-0.5_v2/DDPM_downscale_{args.dataset_type}_ns-{args.noise_schedule}__bs-{args.batch_size}_e-{args.epochs}_lr-{args.lr}_cfg{args.cfg_proportion}_size{args.image_size}_shuffle{args.shuffle}"
+    args.run_name = f"NewTransform_NewSampling_fixed_s-{args.dataset_size}_train-0.5/DDPM_downscale_{args.dataset_type}_ns-{args.noise_schedule}__bs-{args.batch_size}_e-{args.epochs}_lr-{args.lr}_cfg{args.cfg_proportion}_size{args.image_size}_resratio{arg.resolution_ratio}"
     args.interp_mode = 'bicubic'
     args.noise_steps = 750
     args.device = "cuda"
